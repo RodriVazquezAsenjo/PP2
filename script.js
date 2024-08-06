2ca4e9d43c82e71f0b14fc250036a22aef96c4b7
RodriVazquezAsenjo <r.vazquezasenjo@gmail.com>
2024-08-06 21:39:53 +0000

    initial styling for Lists page

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 let groceryList = [];
-
+/*
+* This function will add every third item added to the list
+*/
 function handleSubmit(event) {
     event.preventDefault();
-    let input = document.getElementById('input').value;
-    groceryList.push(input);
+    let ingredient = document.getElementById('ingredient').value;
+    //adds the item to the groceryList array.
+    groceryList.push(ingredient);
     let tbody = document.getElementById('ingredient-list');
     
-    // Update the content of the 'trial' div with the input value
     tbody.innerHTML = '';
-
+    //for loops allows you to add the third item in the groceryList array.
     for (let i=0; i<groceryList.length; i += 3) {
         let row = document.createElement('tr');
         row.innerHTML = `
-            <td>${groceryList[i] || ''}</td>
-            <td>${groceryList[i + 1] || ''}</td>
-            <td>${groceryList[i + 2] || ''}</td>
+            <td class='table-entry'>${groceryList[i] || ''}</td>
+            <td class='table-entry'>${groceryList[i + 1] || ''}</td>
+            <td class='table-entry'>${groceryList[i + 2] || ''}</td>
         `;
         tbody.appendChild(row);
     }
-    document.getElementById('input') = '';
+    //sets the input to blank after pressing enter.
+    document.getElementById('input').value = '';
 }
